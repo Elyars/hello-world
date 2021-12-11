432da8f57910974379ce87579d1b6fd1765f87a3
Elyas <2665369011@qq.com>
2021-12-11 19:14:49 +0800

    静态库的创建和使用
    B20031811伊力亚斯

---
+++ StaticMath/MathLibrary/MathLibrary.h
@@ -0,0 +1,19 @@
+#pragma once
+namespace MathLibrary
+{
+	class Arithmetic
+	{
+	public:
+		//return a+b
+		static double Add(double a, double b);
+
+		//return a-b
+		static double Subtract(double a, double b);
+
+		//return a*b
+		static double Multiply(double a, double b);
+
+		//return a/b
+		static double Divide(double a, double b);
+	};
+}
